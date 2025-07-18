145d938ca02c4b36f1f1afd5208db941368bf86d
Samy <samycaballero014@gmail.com>
2025-07-18 18:57:08 -0500

    models.py y serializers.py actualizados

--- api/serializers.py
+++ api/serializers.py
@@ -1,44 +1,48 @@
 from rest_framework import serializers
+from django.contrib.auth.hashers import make_password
 from .models import (
     Visitante, Sendero, SenderoFoto,
     RegistroVisita, Encuesta,
-    RolUsuario, Usuario, Comentario
+    Usuario, Comentario
 )
+
 
 class VisitanteSerializer(serializers.ModelSerializer):
     class Meta:
         model = Visitante
         fields = '__all__'
 
+
 class SenderoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Sendero
         fields = '__all__'
+
 
 class SenderoFotoSerializer(serializers.ModelSerializer):
     class Meta:
         model = SenderoFoto
         fields = '__all__'
 
+
 class RegistroVisitaSerializer(serializers.ModelSerializer):
     class Meta:
         model = RegistroVisita
         fields = '__all__'
+
 
 class EncuestaSerializer(serializers.ModelSerializer):
     class Meta:
         model = Encuesta
         fields = '__all__'
 
-class RolUsuarioSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = RolUsuario
-        fields = '__all__'
 
 class UsuarioSerializer(serializers.ModelSerializer):
+    rol = serializers.ChoiceField(choices=Usuario.Rol.choices, default=Usuario.Rol.USER)
+
     class Meta:
         model = Usuario
-        exclude = ['contraseña']  # ⚠️ No devolvemos la contraseña hasheada
+        exclude = ['contraseña']  
 
     def create(self, validated_data):
         """Hashear contraseña manualmente al crear usuario"""
@@ -48,6 +52,7 @@ class UsuarioSerializer(serializers.ModelSerializer):
             instance.contraseña = make_password(contraseña)
         instance.save()
         return instance
+
 
 class ComentarioSerializer(serializers.ModelSerializer):
     class Meta:
--- api/views.py
+++ api/views.py
@@ -2,6 +2,7 @@ from rest_framework import viewsets
 from .models import Visitante
 from .serializers import VisitanteSerializer
 
+
 class VisitanteViewSet(viewsets.ModelViewSet):
     queryset = Visitante.objects.all()
     serializer_class = VisitanteSerializer
