7de55741e6ccc06f7950749c13e365f6d546ce30
Relez <randolphjr27@gmail.com>
2025-07-19 12:23:44 -0500

    agregando parte de registro de visitantes y visitante
    enn esto se agrega para que el cliente se pueda registrar y pueda permitir tener un perifl en la cuenta

--- api/views.py
+++ api/views.py
@@ -1,7 +1,11 @@
 from rest_framework import viewsets
-from .models import Visitante
-from .serializers import VisitanteSerializer
+from .models import Visitante, RegistroVisita
+from .serializers import VisitanteSerializer,RegistroVisitaSerializer
 
 class VisitanteViewSet(viewsets.ModelViewSet):
     queryset = Visitante.objects.all()
     serializer_class = VisitanteSerializer
+
+class RegistroVisitaViewSet(viewsets.ModelViewSet):
+    queryset = RegistroVisita.objects.all()
+    serializer_class = RegistroVisitaSerializer
